925068f6c3dcd469178a8024f765520e8dfb8bc3
Charl P. Botha <cpbotha@vxlabs.com>
2023-12-24 09:33:06 +0200

    Add mode to just export to HTML

--- org-roam-to-apple-notes.el
+++ org-roam-to-apple-notes.el
@@ -23,7 +23,7 @@
                        "end tell\n"
                        "end tell\n"))))
                                         ; 
-(defun oran--export-node-to-apple-notes (node temp-dir)
+(defun oran--export-node-to-apple-notes (node temp-dir do-apple-notes)
   "Export the given node as markdown and save it in a file."
   (let* ((file (org-roam-node-file node))
          (id (org-roam-node-id node))
@@ -35,8 +35,8 @@
          ;; silence "Need absolute ‘org-attach-id-dir’ to attach in buffers without filename" error
          (org-attach-directory "/tmp")
 
+         ;; we embed images as base64 in the forlorn hope that Apple Notes might show these
          ;; https://emacs.stackexchange.com/questions/27060/embed-image-as-base64-on-html-export-from-orgmode
-         ;; we need self-contained to include images as base64
          (org-pandoc-options-for-html5 '((standalone . t) (self-contained . t)))
          )
 
@@ -61,49 +61,48 @@
         (let ((default-directory temp-dir))
           (message "----- default directory %s" default-directory)
           (setq oran-html-fn nil)
-          (setq oran-html-fn (expand-file-name (plist-get (org-pandoc-export-to-html5 nil (org-at-heading-p) nil nil) 'output-file))))
+          (setq oran-html-fn (expand-file-name (plist-get (org-pandoc-export-to-html5 nil (org-at-heading-p) nil nil) 'output-file)))
 
-        ;; wait for pandoc to actually finish
-        ;; not even sure if this is necessary anymore.
-        ;; I did this because Apple Notes kept on failing to import embedded images,
-        ;; and it looked like it was being given incomplete HTML files. However, in the
-        ;; end it seems that Apple Notes's applescript support is just really really sad.
-        ;; TBF, before this wait, I did see the HTML export success message AFTER the new note was generated
-        (setq wait-time 0)
-        (while (and (not oran-html-done) (< wait-time 3))
-          (sleep-for 0.1)
-          ;; if pandoc errors out, pandoc-hook is never called, and pandoc also
-          ;; sever raises an error or anything (only message), so we have to
-          ;; have our own little timeout as well
-          (setq wait-time (+ wait-time 0.1)))
+          ;; wait for pandoc to actually finish
+          ;; not even sure if this is necessary anymore.
+          ;; I did this because Apple Notes kept on failing to import embedded images,
+          ;; and it looked like it was being given incomplete HTML files. However, in the
+          ;; end it seems that Apple Notes's applescript support is just really really sad.
+          ;; TBF, before this wait, I did see the HTML export success message AFTER the new note was generated
+          (setq wait-time 0)
+          (while (and (not oran-html-done) (< wait-time 3))
+            (sleep-for 0.1)
+            ;; if pandoc errors out, pandoc-hook is never called, and pandoc also
+            ;; sever raises an error or anything (only message), so we have to
+            ;; have our own little timeout as well
+            (setq wait-time (+ wait-time 0.1))))
 
         (if (file-exists-p oran-html-fn)
             (progn
-              ;; get size of oran-html-fn on disk
-              (setq oran-html-size (nth 7 (file-attributes oran-html-fn)))
-              (message "oran-html-size: %s" oran-html-size)
-              
-              (message "creating the note! %s" oran-html-fn)
-              (org-mac-link-do-applescript
-               (concat
-                "set BODY_FN to (the POSIX path of \"" oran-html-fn "\")\n"
-                "set NBODY to read BODY_FN\n"
-                "tell application \"Notes\"\n"
-                ;;"activate\n"
-                "tell folder \"org-roam\"\n" 
-                "if not (note named \"" title "\" exists) then\n"
-                "make new note with properties {body:NBODY}\n"
-                "end if\n"
-                "end tell\n"
-                "end tell\n"
-                )))
-          nil)
-
-
-        ))))
+              (when do-apple-notes
+                ;; get size of oran-html-fn on disk
+                (setq oran-html-size (nth 7 (file-attributes oran-html-fn)))
+                (message "oran-html-size: %s" oran-html-size)
+                
+                (message "creating the note! %s" oran-html-fn)
+                (org-mac-link-do-applescript
+                 (concat
+                  "set BODY_FN to (the POSIX path of \"" oran-html-fn "\")\n"
+                  "set NBODY to read BODY_FN\n"
+                  "tell application \"Notes\"\n"
+                  ;;"activate\n"
+                  "tell folder \"org-roam\"\n" 
+                  "if not (note named \"" title "\" exists) then\n"
+                  "make new note with properties {body:NBODY}\n"
+                  "end if\n"
+                  "end tell\n"
+                  "end tell\n")))
+              ;; if HTML export was successful, we return the full filename
+              oran-html-fn)
+          nil)))))
 
 ;;;###autoload
-(defun oran-export-org-roam-nodes ()
+(defun oran-export-org-roam-nodes-to-apple-notes ()
   (interactive)
   (let ((temp-dir (make-temp-file "org-roam-" t))
         (node-list (org-roam-node-list)))
@@ -119,7 +118,25 @@
             (error (message "ERROR exporting %s: %s" (org-roam-node-title node) err))))))
     (message "DONE.")))
 
+;;;###autoload
+(defun oran-export-org-roam-nodes-to-temp-dir ()
+  (interactive)
+  (let ((temp-dir (make-temp-file "org-roam-" t))
+        (node-list (org-roam-node-list)))
+    (message "======> exporting %d nodes" (length node-list))
+    (dolist (node node-list)
+      (message "exporting %s" (org-roam-node-title node))
+      ;; catch error from export, report, but continue to next node
+      (condition-case err
+          (oran--export-node-to-apple-notes node temp-dir)
+        (error (message "ERROR exporting %s: %s" (org-roam-node-title node) err))))
+    (message "DONE.")
+    temp-dir))
 
+;;;###autoload
+(defun oran-export-this-node-to-tmp ()
+  (interactive)
+  (oran--export-node-to-apple-notes (org-roam-node-at-point) "/tmp" nil))
 
 (provide 'org-roam-to-apple-notes)
 
